85f980d271735ce44238295bbea38281873e62df
ali-allaw <108570555+ali-allaw@users.noreply.github.com>
2022-08-23 15:36:44 +0300

    files added

---
+++ main.js
@@ -0,0 +1,39 @@
+// localStorage.setItem("lastname", "Smith");
+// localStorage.getItem("lastname");
+
+let input = document.querySelector(".input");
+let submit = document.querySelector(".add");
+let tasks = document.querySelector(".tasks");
+let task = document.querySelector(".tasks .task")
+let inputValue = input.value
+
+if (input.value !== "") {
+    input.onblur = function () {
+        localStorage.setItem("input", input.value);
+    };
+    input.value = localStorage.getItem("input");
+}
+
+submit.onclick = function () {
+    if (input.value != "") {
+        let div = document.createElement("div")
+        let del = document.createElement("p")
+        let delText = document.createTextNode("delete")
+        del.appendChild(delText)
+        div.classList.add("task")
+        div.innerHTML  = input.value
+        tasks.appendChild(div)
+        input.value = "";
+        div.onclick = function () {
+        this.classList.add("done")
+        }
+        myArr.push(div)
+        localStorage.setItem("tasks", myArr)
+        div.appendChild(del)       
+        del.onclick = function () {
+            del.parentElement.remove()
+        }
+    }
+};
+
+//(tasks.children)
